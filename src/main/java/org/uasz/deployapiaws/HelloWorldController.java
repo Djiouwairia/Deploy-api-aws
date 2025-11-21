d124aebdef81f8e93207fccdbaccd9a52100c6e0
Djiouwairiatou Ba <d.b43@zig.univ.sn>
2025-11-21 16:22:22 +0000

    first commit

--- src/main/java/org/uasz/deployapiaws/HelloWorldController.java
+++ src/main/java/org/uasz/deployapiaws/HelloWorldController.java
@@ -1,4 +1,13 @@
 package org.uasz.deployapiaws;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldController {
+
+    @RequestMapping("/HelloWorld")
+    public String helloWorld(){
+        return "Hello World";
+    }
 }
--- src/test/java/org/uasz/deployapiaws/DeployApiAwsApplicationTests.java
+++ src/test/java/org/uasz/deployapiaws/DeployApiAwsApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DeployApiAwsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
